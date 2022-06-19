71a0dd0cde136ec25b908db06d4862980bc21ffe
imAnil09 <bosianils143@gmail.com>
2022-06-19 23:33:35 +0530

    Adding a Game to my repo

---
+++ script.js
@@ -0,0 +1,50 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '🛑 No Number ';
+  } else if (secretNumber === guess) {
+    document.querySelector('.message').textContent = '🎉Correct Number';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      score = highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = `📈 It's too High`;
+      score--;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = `📉 It's too low`;
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '😭 You Lose';
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = 'red';
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', () => {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
